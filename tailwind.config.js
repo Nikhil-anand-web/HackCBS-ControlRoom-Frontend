493822f7cd2e365cb748f026bcdebcc1130a63f1
na52m2002 <na52m2002@gmail.com>
2023-11-05 10:38:27 +0530

    left over

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        SpaceMono: ["monospace"],
+      },
+      dropShadow: {
+        glow: [
+          "0 0px 20px rgba(255,255, 255, 4)",
+          "0 0px 65px rgba(255, 255,255, 4)",
+        ],
+      },
+    },
   },
   plugins: [],
 };
